100d20ce24814e5798edaf3123a7fced05bb20ae
RobotronOmega <kevincperry@hotmail.com>
2025-07-22 08:27:42 -0400

    player ship rotation and movement, configured groups for multiple objects

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
+        # quit handler
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # input handling
+        updatable.update(dt)
+
+        # render screen    
         screen.fill("black")
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
